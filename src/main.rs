e48470a71ed2b39d2d198b4712d13c05f5b57584
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-11-25 20:05:38 +0100

    tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for streams in listener.incoming() {
+        let stream = streams.unwrap();
+        println!("Connection Established");
+    }
 }
